29ba855f9d16b8404994b51f0feedde29c7ed036
Ei Ngoan Phoo <eingoan38@gmail.com>
2024-10-23 20:21:41 +0630

    My Java Practice first commit

--- refrencedatatypetest/src/refrencedatatype/JavaDataType.java
+++ refrencedatatypetest/src/refrencedatatype/JavaDataType.java
@@ -1,8 +1,47 @@
 package refrencedatatype;
+import java.util.*;
 
 public class JavaDataType {
     public static void main(String [] args){
-        int i = 10;
-        //Student s = new Student();
+
+        byte b ;
+        short s;
+        int i ;
+        long l=0;
+        //Student student = new Student(); public class Student{}
+        //l = i; //implicit casting  memory size greater than <= memory size less than
+        b = (byte)l;//explicit casting
+        // takeValue((byte)10);
+        //takeValue((short)10);
+        // takeValue(10);
+        //takeValue(10l);
+
+        b = (byte)l;
+        System.out.println(b);
+
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
     }
+    static void takeValue(byte b){
+        System.out.println("Byte is working");
+    }
+
+    static void takeValue(short s){
+        System.out.println("Short is working");
+    }
+
+    static void takeValue(int i){
+        System.out.println("Int is working");
+    }
+
+    static void takeValue(long l){
+        System.out.println("Long is working");
+    }
+
+
+
+
 }
