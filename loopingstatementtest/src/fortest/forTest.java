dae4ad4e27df172aae9b82a5401b75afffe5a30b
Ei Ngoan Phoo <eingoan38@gmail.com>
2024-10-28 18:54:14 +0630

    My Basic Java Practice first commit

---
+++ loopingstatementtest/src/fortest/forTest.java
@@ -0,0 +1,17 @@
+package fortest;
+import java.util.*;
+public class forTest {
+    /* public static void main(String[] args){
+        int [] arr = {1,2,3,4,5};
+        for (int i = 0; i< arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }*/
+
+    //Write a for loop to display every 3rd number, starting at 10 and ending at 30.
+    /* public static void main(String [] args){
+        for(int i =10; 10<= i && i<=30;i+=3){
+            System.out.println(i);
+        }
+    }*/
+}
