be3e2390e4913174938a8eaaa4548db243b0b0d1
Ei Ngoan Phoo <eingoan38@gmail.com>
2024-10-29 11:56:44 +0630

    My Basic Java Practice first commit

--- loopingstatementtest/src/fortest/forTest.java
+++ loopingstatementtest/src/fortest/forTest.java
@@ -13,5 +13,32 @@ public class forTest {
         for(int i =10; 10<= i && i<=30;i+=3){
             System.out.println(i);
         }
-    }*/
+    }
+    ********************************************************************/
+
+    /*Write a for loop that iterates through each number between 1 and 6, and
+    uses a modulus operator along with an if statement to state whether each
+    value is even or odd.
+
+    public static void main(String [] args){
+        for (int i=1; 1<=i && i<=6;i++ ){
+            if(i %2 == 0) System.out.println(i + " is even");
+            else System.out.println(i+" is odd");
+        }
+    }
+    *******************************************************************/
+
+    /*Write a program that will take in a start number and an end number from a
+    user. Then, using a while loop show all the values from the start to the end
+    number, (inclusive).
+
+    public static void main(String [] args){
+        int input1,input2;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a start number: ");input1=sc.nextInt();
+        System.out.print("Enter an end number: ");input2=sc.nextInt();
+        for(int i=input1; i<=input2;i++) System.out.print(i);
+    }
+    ********************************************************************/
+
 }
